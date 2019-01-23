5bd8375b622819865d6041eebe723c1533208743
RDMay <46907844+RDMay@users.noreply.github.com>
2019-01-22 19:59:02 -0500

    J

--- slide.js
+++ slide.js
@@ -1,1 +1,18 @@
 slide = new XMLHttpRequest();
+slide.open("GET", "slide.svg",false);
+slide.overrideMimeType("image/svg+xml");
+slide.send("");
+var slide = document.getElementById("main").appendChild(slide.responseXML.documentElement);
+
+
+var svgWindow = document.getElementById("main");
+var svg = d3.select(slide);
+function redraw(){
+	var width = svgWindow.clientWidth;
+	var height = svgWindow.clientHeight;
+	svg.attr("width", width)
+	.attr("height", height);
+	console.log("fired")
+}
+redraw();
+window.addEventListener("resize", redraw)
