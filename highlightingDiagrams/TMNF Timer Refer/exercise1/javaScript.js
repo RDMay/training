b6d4a25f3293fed789266b2a97a3b0b3357dba19
May <Robert.May@geappliances.com>
2021-02-17 08:14:27 -0500

    J

--- highlightingDiagrams/TMNF Timer Refer/exercise1/javaScript.js
+++ highlightingDiagrams/TMNF Timer Refer/exercise1/javaScript.js
@@ -144,49 +144,49 @@ speedControlArrayKey = [];
 function checkAnswer(){
   getColors();
   var answerArray = [];
-  for(a=0; a<l1ArrayKey.length; a++){
-    if(l1ArrayKey.includes(l1Array[a]) == false || l1Array.length != l1ArrayKey.length){
-      answerArray.push("false")
-    }
-  }
-  for(a=0; a<neutralArrayKey.length; a++){
-    if(neutralArrayKey.includes(neutralArray[a]) == false || neutralArray.length != neutralArrayKey.length){
-      answerArray.push("false")
-    }
-  }
-  for(a=0; a<dcPlusArrayKey.length; a++){
-    if(dcPlusArrayKey.includes(dcPlusArray[a]) == false || dcPlusArray.length != dcPlusArrayKey.length){
-      answerArray.push("false")
-    }
-  }
-  for(a=0; a<dcMinusArrayKey.length; a++){
-    if(dcMinusArrayKey.includes(dcMinusArray[a]) == false || dcMinusArray.length != dcMinusArrayKey.length){
-      answerArray.push("false")
-    }
-  }
-  for(a=0; a<rpmFeedBackArrayKey.length; a++){
-    if(rpmFeedBackArrayKey.includes(rpmFeedBackArray[a]) == false || rpmFeedBackArray.length != rpmFeedBackArrayKey.length){
-      answerArray.push("false")
-    }
-  }
-  for(a=0; a<speedControlArrayKey.length; a++){
-    if(speedControlArrayKey.includes(speedControlArray[a]) == false || speedControlArray.length != speedControlArrayKey.length){
-      answerArray.push("false")
-    }
-  }
+  // for(a=0; a<l1ArrayKey.length; a++){
+  //   if(l1ArrayKey.includes(l1Array[a]) == false || l1Array.length != l1ArrayKey.length){
+  //     answerArray.push("false")
+  //   }
+  // }
+  // for(a=0; a<neutralArrayKey.length; a++){
+  //   if(neutralArrayKey.includes(neutralArray[a]) == false || neutralArray.length != neutralArrayKey.length){
+  //     answerArray.push("false")
+  //   }
+  // }
+  // for(a=0; a<dcPlusArrayKey.length; a++){
+  //   if(dcPlusArrayKey.includes(dcPlusArray[a]) == false || dcPlusArray.length != dcPlusArrayKey.length){
+  //     answerArray.push("false")
+  //   }
+  // }
+  // for(a=0; a<dcMinusArrayKey.length; a++){
+  //   if(dcMinusArrayKey.includes(dcMinusArray[a]) == false || dcMinusArray.length != dcMinusArrayKey.length){
+  //     answerArray.push("false")
+  //   }
+  // }
+  // for(a=0; a<rpmFeedBackArrayKey.length; a++){
+  //   if(rpmFeedBackArrayKey.includes(rpmFeedBackArray[a]) == false || rpmFeedBackArray.length != rpmFeedBackArrayKey.length){
+  //     answerArray.push("false")
+  //   }
+  // }
+  // for(a=0; a<speedControlArrayKey.length; a++){
+  //   if(speedControlArrayKey.includes(speedControlArray[a]) == false || speedControlArray.length != speedControlArrayKey.length){
+  //     answerArray.push("false")
+  //   }
+  // }
   for(a=0; a<energizedLoadArrayKey.length; a++){
     if(energizedLoadArrayKey.includes(energizedLoadArray[a]) == false || energizedLoadArray.length != energizedLoadArrayKey.length){
       answerArray.push("false")
     }
   }
 
-  if(ccSwitchRotated === true){
-    answerArray.push("false")
-  }
-
-  if(timerSwitchRotated === true){
-    answerArray.push("false")
-  }
+  // if(ccSwitchRotated === true){
+  //   answerArray.push("false")
+  // }
+  //
+  // if(timerSwitchRotated === true){
+  //   answerArray.push("false")
+  // }
 
   if(answerArray.includes('false')){
     // console.log(answerArray)
